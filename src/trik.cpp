8ac72918f6636c0487a16d05ae5302631d685780
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2016-03-06 13:21:10 +0300

    Add brick interface

--- src/trik.cpp
+++ src/trik.cpp
@@ -1,12 +1,18 @@
 #include "ros/ros.h"
 #include "nodelet/nodelet.h"
 #include "pluginlib/class_list_macros.h"
+#include "trikControl/brickInterface.h"
+#include "trikControl/brickFactory.h"
 
 class TrikNode : public nodelet::Nodelet {
 public:
   void onInit() {
     NODELET_DEBUG("Initializing TrikNode...");
+    brick = trikControl::BrickFactory::create();
   }
+
+private:
+    trikControl::BrickInterface *brick;
 };
 
 PLUGINLIB_EXPORT_CLASS(TrikNode, nodelet::Nodelet)
